ec760746c72ff3c740be0da92e4b12e0c12be6bd
Alice Alessandrini <alice.alessandrini.12.12.1993@gmail.com>
2024-02-02 15:58:48 +0100

    Aggiungere i file di progetto.

---
+++ ImposteApp/Contribuente.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImposteApp
+{
+    internal class Contribuente
+    {
+        // DEVE AVERE LE CLASSI
+        //Nome, Cognome, DataNascita, CodiceFiscale, Sesso, ComuneResidenza, RedditoAnnuale
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateTime DataNascita { get; set; }
+        public string CodiceFiscale { get; set; }   
+        public char Sesso { get; set; }   
+        public string ComuneResidenza { get; set; } 
+        public double RedditoAnnuale { get; set; }
+        private double Imposta {  get; set; }
+
+        // euro symbol:
+        char euro = '\u20AC';
+
+        // Costruttore Contribuente:
+        public Contribuente(string nome, string cognome, DateTime dataNascita, string codiceFiscale, char sesso, string comuneResidenza, double redditoAnnuale)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = dataNascita;
+            CodiceFiscale = codiceFiscale;
+            Sesso = sesso;
+            ComuneResidenza = comuneResidenza;
+            RedditoAnnuale = redditoAnnuale;
+        }
+        // Metodi:
+        // Riepilogo dati che apparirà nel menù utente dopo l'inserimento dei dati per la conferma:
+        public void Riepilogo()
+        {
+            Console.WriteLine($"Contribuente:\t\t\t{Nome} {Cognome}");
+            Console.WriteLine($"Nato il:\t{DataNascita} ({Sesso})");
+            Console.WriteLine($"Residente in:\t{ComuneResidenza}");
+            Console.WriteLine($"Codice Fiscale:\t\t{CodiceFiscale}");
+            Console.WriteLine($"Reddito dichiarato:\t {RedditoAnnuale}");
+        }
+
+        // Calcolo imposta:
+        // dato RedditoAnnuale 
+        // ritorna un double (imposta)
+        public double CalcolaImposta()
+        {
+            Console.WriteLine("================ CALCOLO IMPOSTA ============== \n");
+
+            switch (RedditoAnnuale)
+            {
+                case double reddito when reddito <= 15000:
+                    Imposta = reddito * 0.23;
+                    break;
+
+                case double reddito when reddito <= 28000:
+                    Imposta = 3450 + (reddito - 15000) * 0.27;
+                    break;
+
+                case double reddito when reddito <= 55000:
+                    Imposta = 6960 + (reddito - 28000) * 0.38;
+                    break;
+
+                case double reddito when reddito <= 75000:
+                    Imposta = 17220 + (reddito - 55000) * 0.41;
+                    break;
+
+                default:
+                    Imposta = 25420 + (RedditoAnnuale - 75000) * 0.43;
+                    break;
+            }
+
+            RecapImposta();
+            return Imposta;
+        }
+
+        public void RecapImposta() {
+            Console.WriteLine("================ CALCOLO IMPOSTA ============== \n");
+            Riepilogo();
+            Console.WriteLine($"IMPOSTA DA VERSARE: \t{Imposta}");
+            Console.WriteLine("\n\n");
+
+        }
+    }
+}
---
+++ ImposteApp/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImposteApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prima istanza di choice (scelta del menù) e di NuovoContribuente
+            int choice;
+            Contribuente NuovoContribuente = null;
+
+            // inizio ciclo do-while cosicchè il programma si ripeta finché non si esce o si prema una key diversa
+            do
+            {
+                // MENU OPERAZIONI UTENTE
+                Console.WriteLine("================MENU OPERAZIONI UTENTE==============");
+                Console.WriteLine("Scegli l'operazione da effettuare:");
+                Console.WriteLine("1. Inserisci dati");
+                                                         // Il calcolo dell'imposta ora avviene direttamente dopo aver confermato i dati 
+                Console.WriteLine("2. Esci");
+                Console.WriteLine("========================================");
+                
+                // Scelta basata dall'input dell'utente:
+                choice = Convert.ToInt32(Console.ReadLine()); // valore di choice
+                //--------------------------------------------------------------
+                // visto che sono solo e opzioni, con if inserisce i dati + costruttore + dopo la conferma dei dati avvia metodo per calcolare l'imposta
+                                                   // 2 chiude il programma
+                                                   // Con qualsiasi altra key riavvia il programma (do-while)
+                if (choice == 1)
+                {
+                    // Inserimento dei dati Contribuente:
+                    Console.WriteLine("\nInserisci nome:");
+                    string nome = Console.ReadLine();
+                    Console.WriteLine("\nInserisci cognome:");
+                    string cognome = Console.ReadLine();
+                    //-------------------------------------------------------------------------------
+                    // Data di nascita + controllo data nel formato giusto
+                    Console.WriteLine("\nInserisci data di nascita (gg/mm/aaaa):");
+                    string inputDate = Console.ReadLine();
+                    DateTime dataNascita;
+                    while (!DateTime.TryParseExact(inputDate, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out dataNascita))
+                    {
+                        Console.WriteLine("Data non valida, riprova:");
+                        inputDate = Console.ReadLine();
+                    }
+                    //-------------------------------------------------------------------------------
+                    // Codice fiscale + controllo sul codice fiscale
+                    Console.WriteLine("\nInserisci il tuo codice fiscale:");
+                    string codiceFiscale = Console.ReadLine();
+                    if (codiceFiscale.Length != 16)
+                    {
+                        Console.WriteLine("\nIl codice fiscale deve essere di 16 caratteri!\n");
+                        Console.WriteLine("Riprova:\n");
+                        codiceFiscale = Console.ReadLine();
+                    }
+                    //-------------------------------------------------------------------------------
+                    // Sesso + controllo sul sesso con bonus inclusività
+                    Console.WriteLine("\nInserisci sesso (m/f/a):");
+                    char sesso = Console.ReadKey().KeyChar;
+                    if (sesso != 'f' || sesso != 'm' || sesso != 'a') 
+                    {
+                        Console.WriteLine("\nCarattere non valido.");
+                        Console.WriteLine("Riprova:\n");
+                        sesso = Console.ReadKey().KeyChar;
+                    }
+                    // -------------------------------------------------------------------------------
+                    Console.WriteLine("\nInserisci il comune di residenza:");
+                    string comuneResidenza = Console.ReadLine();
+                    //--------------------------------------------------------------------------------
+                    // Reddito annuale + controllo che siano numeri
+                    Console.WriteLine("\nInserisci il tuo reddito annuale:");
+                    double redditoAnnuale;
+                    while (!double.TryParse(Console.ReadLine(), out redditoAnnuale))
+                    {
+                        Console.WriteLine("Inserisci un valore numerico valido.");
+                        Console.WriteLine("\nInserisci il tuo reddito annuale:");
+                    }
+                    //-------------------------------------------------------------------------------
+                    // Parte il costruttore:
+                    NuovoContribuente = new Contribuente(nome, cognome, dataNascita, codiceFiscale, sesso, comuneResidenza, redditoAnnuale);
+                    //
+                    //------------------ RIEPILOGO E CONFERMA DEI DATI --------------------------------------
+                    Console.WriteLine("================ RIEPILOGO DATI ==============\n");
+                    NuovoContribuente?.Riepilogo(); // mostra il riepilogo SOLO se NuovoContribuente non è null
+                    Console.WriteLine("Confermi i dati inseriti? (y/n)");
+                    char conferma = Console.ReadKey().KeyChar;
+                    if (conferma == 'n' || conferma == 'N')
+                    {
+                        Console.WriteLine("\n Il menù verrà riavviato \n");
+                        NuovoContribuente = null;
+                        //checkConferma = false;
+                    }
+                    else if (conferma == 'y' || conferma == 'Y')
+                    {
+                        Console.WriteLine("\nAvviando il menù per il calcolo dell'imposta...\n");
+                        // checkConferma = true;
+                        // choice = 2; //nb.: anche forzando il valore di choice a 2 non entra nel blocco else if
+                        NuovoContribuente?.CalcolaImposta();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nRiavvio del programma...\n");
+                }
+
+            } while (choice != 2); // Continua fino a quando la scelta non è "Esci" (5)
+        }
+    }
+}
